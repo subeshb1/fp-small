119e3db15e826a0bc8cc2196a7fcea184d5a99c0
subeshb1 <subeshb1@gmail.com>
2018-10-14 12:21:27 +0545

    test(fp.test.js): Add Test for fp.js
    Added tests for curry and compose

---
+++ src/fp/fp.test.js
@@ -0,0 +1,33 @@
+import { curry, compose } from ".";
+
+describe("Testing Curry", () => {
+  it("curries add function", () => {
+    const mock = jest.fn((a, b) => a + b);
+    const curryAdd = curry(mock);
+    const add1 = curryAdd(1);
+    const onePlusTwo = curryAdd(1)(2);
+
+    expect(add1(10)).toBe(11);
+    expect(onePlusTwo).toBe(3);
+
+    expect(mock.mock.calls.length).toBe(2);
+    expect(mock.mock.calls[0][1]).toBe(2);
+    expect(mock.mock.calls[1][1]).toBe(10);
+
+    expect(curry).toThrow();
+    
+  });
+});
+
+
+describe('Testing Compose', () => {
+    it('composes functions', () => {
+        const add1 = x => x+1;
+        const mul2 = x => x*2;
+        const sub1 = x => x - 1;
+
+        const add1Mul2Sub1  = compose(sub1,mul2,add1);
+
+        expect(add1Mul2Sub1(10)).toBe(sub1(mul2(add1(10))));
+    });
+});

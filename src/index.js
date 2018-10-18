ceb9150bc4fad45ef30b8372bad217fb863f8b63
sketch-tester <subeshb123@gmail.com>
2018-10-18 04:03:31 +0000

    fix(Either): Changed Import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { default as Task } from "./Task";
-export { default as Either } from "./Either";
+export * from "./Either";
 export { default as Maybe } from "./Maybe";
 export * from "./fp";
